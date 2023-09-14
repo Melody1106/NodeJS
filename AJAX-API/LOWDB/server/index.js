c60a49fcfcafd13c5548a97eb43a1f7af3272ed7
Melody1106 <101308244+Melody1106@users.noreply.github.com>
2023-09-14 12:06:27 +0800

    restful-API-01

---
+++ AJAX-API/LOWDB/server/index.js
@@ -0,0 +1,241 @@
+const express = require("express");
+const multer = require("multer");
+const moment = require("moment");
+const cors = require("cors");
+//json web token
+const jwt = require('jsonwebtoken');
+const secretKey = "benbenbenIamben";
+const upload = multer();
+//loadDB導入
+const { v4: uuidv4 } = require('uuid');
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+// 設定部份
+let whitelist = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+    undefined
+];
+let corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+const app = express()
+app.use(cors(corsOptions))
+//解析 json 格式的要求主體
+app.use(express.json())
+//解析 URL 編碼的要求主體
+app.use(express.urlencoded({extended:true}))
+
+// 路由部份
+app.get("/", (req, res)=>{
+    res.send("首頁");
+  });
+
+//使用者登入
+app.post("/api/users/login", upload.none(),async (req, res)=>{
+   //console.log(req.body);
+    // res.status(200).json(
+    //         { "message": "Login successful", "token": "abc123xyz"}
+    // )
+    let user, error
+    await usersLogin(req).then(result=>{
+        user = result;
+    }).catch(err=>{
+       // console.log(err);
+        error = err;
+    
+    });
+    if(error){
+        res.status(400).json({error});
+        return false;
+    }
+    //如果登入成功發token
+    const token = jwt.sign(
+        {
+            account: user.account,
+            name: user.name,
+            head: user.head
+        },
+        secretKey, 
+        {expiresIn: "30m"});
+    res.status(200).json(
+        { message: "Login successful", 
+             token: token
+            })
+  });
+//使用者登出
+app.post("/api/users/logout",checkToken, async (req, res)=>{
+    const token = jwt.sign(
+        {
+            account: req.decoded.account,
+            name: req.decoded.name,
+            head: req.decoded.head
+        }, 
+        secretKey, 
+        {expiresIn: "-10s"});
+    res.status(200).json(
+        {
+            message: "Logout successful", 
+            token: token
+        });
+});
+//檢查使用者登入狀態
+app.post("/api/users/status",checkToken,(req, res)=>{
+    const token = jwt.sign(
+        {
+            account: req.user.account,
+            name: req.user.name,
+            head: req.user.head
+        },
+        secretKey, 
+        {expiresIn: "30m"});
+   
+    res.status(200).json(
+            { message: "認證有效",
+             token: token}
+    )
+  });
+//新增一個使用者
+app.post("/api/users", upload.none(),async(req, res)=>{
+    let user, error;
+    await usersAdd(req).then(result=>{
+        user = result;
+    }).catch(err=>{
+        err = error;
+    })
+    if(error){
+        res.status(400).json(
+            {error}
+    )
+        return false;
+    }
+    //成功的回應
+    res.status(201).json(
+            {"id":user.id, 
+            "message": "User created successfully"}
+    )
+
+  });
+ 
+  //修改特定 ID 使用者的部分內容
+  app.put("/api/users/:id",upload.none(), (req, res)=>{
+    let id = req.params.id;
+    console.log(req.body);
+    res.status(200).json(
+       { "message": "User partially updated successfully"}
+    )
+  });
+
+   //刪除特定 ID 的使用者
+   app.delete("/api/users/:id", (req, res)=>{
+    let id = req.params.id;
+    console.log(req.body);
+    res.status(200).json(
+       { "message": "User deleted successfully"}
+    )
+  });
+
+
+  app.get("/api/users", (req, res)=>{
+    res.status(200).json(
+        [
+            {"id": 1, "name": "Alice", "email": "alice@example.com"},
+            {"id": 2, "name": "Bob", "email": "bob@example.com"}
+          ]
+    )
+  });
+
+   //使用 ID 作為搜尋條件來搜尋使用者
+   app.get("/api/users/search", (req, res)=>{
+    let account = req.query.account;
+    res.status(200).json(
+        [
+            {"id": account, "name": "Alice", "email": "alice@example.com"}
+          ]
+    )
+  });
+
+  app.get("/api/users/:id", (req, res)=>{
+    let id = req.params.id;
+    res.status(200).json(
+        [
+            {"id": id, "name": "Alice", "email": "alice@example.com"}
+          ]
+    )
+  });
+
+ 
+  app.listen(3000, ()=>{
+    console.log("server is running");
+  });
+
+function usersAdd(req){
+    return new Promise((resolve, reject)=>{
+        const {account, password, name, head} = req.body;
+        //檢查使用者是否已存在
+     let result =  db.get("user").find({account}).value()
+     if(result){
+        reject({error: "帳號已經被使用"})
+        return false;
+     }
+        let id = uuidv4();
+        db.get("user").push({id,account, password, name, head}).write();
+        resolve({id});
+    })
+}
+
+function usersLogin(req){
+return new Promise((resolve, reject)=>{
+  const {account, password} = req.body
+  let result = db.get("user").find({account, password}).value();
+//如果有user
+if(result){
+    resolve(result)
+    //console.log(result);
+}else{
+    reject({error: "帳號或密碼錯誤"})
+}
+})
+
+    // console.log(req.body);
+    // res.status(200).json(
+    //         { "message": "Login successful", "token": "abc123xyz"}
+    // )
+}
+
+  //建立路由middleware 帶入參數
+function checkToken(req, res, next){
+    //從logout header請求
+    const token = req.headers.authorization
+    //console.log(token);
+    if(token){
+      jwt.verify(token, secretKey, (error, decoded)=>{
+        if(error){
+          res.status(400).json({
+            status: "fail",
+            message:"登入驗證失效 請重新登入"})
+          return false;
+        }
+        //成功要做的事
+        req.decoded = decoded
+        next();
+      })
+    }else{
+      res.status(400).json(
+        {
+            error:"登入驗證失效 請重新登入"
+    })
+    }
+ }
--- lowdb-test01/index.js
+++ lowdb-test01/index.js
@@ -5,6 +5,7 @@ const adapter = new FileSync("db.json");
 const db = low(adapter);
 // db.defaults({ products: [], user: {} }).write();
 
+//寫入
 // db.get("products")
 //     .push({
 //         id: uuidv4(),
@@ -20,7 +21,7 @@ const db = low(adapter);
 //     .set("users.name", "Valwood Pkwy")
 //     .set("users.head", "https://randomuser.me/api/portraits/men/64.jpg")
 //     .write();
-
+//用物件建立資料
 // let id = uuidv4();
 // let user = {
 //     id: id,
@@ -47,6 +48,24 @@ const db = low(adapter);
 //     .value()
 //     console.log(data);
 
-//分頁-每頁5筆
-let data = db.get("products").slice(0).take(5).value();
-console.log(data);
+//分頁-.slice代表取的第幾頁 .take每頁5筆
+// let data = db.get("products").slice(0).take(5).value();
+// console.log(data);
+
+
+//排序 小-大 a-b
+// let data = db.get("products").sort((a,b)=>a.price - b.price).value();
+// console.log(data);
+
+// let data = db.get("products").sort((a,b)=>a.price - b.price)
+// .slice(0).take(5).value();
+
+// let data = db.get("products").slice(0).take(5).sort((a,b)=>a.price - b.price).value()
+
+//修改
+// let data = db.get("products").find({id: "f3e3b921-3b56-4894-8602-aa323c26697a"}).assign({stock: 10}).write();
+
+//刪除
+// let data = db.get("products").remove({id: "bf820195-e2b6-475e-abd1-027929bd30ed"}).write();
+
+// console.log(data);
