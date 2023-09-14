3310aab7854cd201044d3935ba801319a955848e
Melody1106 <101308244+Melody1106@users.noreply.github.com>
2023-09-14 22:57:05 +0800

    client端-api串接

--- AJAX-API/LOWDB/client/src/context/AuthContext.js
+++ AJAX-API/LOWDB/client/src/context/AuthContext.js
@@ -1,7 +1,9 @@
 import React, {createContext, useState} from "react";
 
+//新增一組context
 export const AuthContext = createContext({user: undefined});
 
+//製作一組provider,包住子層物件的內容
 export const AuthProvider = ({children})=>{
     const [user, setUser] = useState(undefined);
     const [token, setToken] = useState(null);
--- AJAX-API/LOWDB/client/src/hooks/useAuth.js
+++ AJAX-API/LOWDB/client/src/hooks/useAuth.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import jwt from "jwt-decode";
 
 const useAuth = ()=>{
+    //取用AuthContext裡面內容
     const {setUser, token, setToken} = useContext(AuthContext);
     const appKey = "benbenbenTest01Token";
     const API_LOGIN = "http://localhost:3005/api/users/login";
@@ -12,12 +13,15 @@ const useAuth = ()=>{
     const login = async (account, password)=>{
         try{
             const result = await axios.post(API_LOGIN, {account, password});
+            //console.log(result)
+            //反組譯
             setToken(result.data.token);
             const u = jwt(result.data.token);
             setUser(u);
             localStorage.setItem(appKey, result.data.token);
         }catch(error){
-            //alert(error.response.data.error.error);
+            //console.log(error)
+            alert(error.response.data.error.error);
         }
     };
 
